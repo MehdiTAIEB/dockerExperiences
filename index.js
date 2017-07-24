74a5c453ae9c8fed12eee68096bbc3756fb26dd5
mehdi <mehdi.taieb@epitech.eu>
2017-07-24 13:02:59 +0200

    node connected to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('mdr');
+const express = require('express')
+const app = express()
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+
+var MONGO_DB;
+var DOCKER_DB = process.env.DB_PORT;
+
+if ( DOCKER_DB ) {
+  MONGO_DB = DOCKER_DB.replace( 'tcp', 'mongodb' ) + '/app';
+} else {
+  MONGO_DB = process.env.MONGODB;
+}
+mongoose.connect(MONGO_DB);
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-echo 'mehdi';
+npm start
